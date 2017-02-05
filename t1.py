3e197231b463bdf743333110b263244a121d9b87
kdratchinsky <kdratchinsky@gmail.com>
2017-02-05 15:57:08 -0800

    Python tutorials

---
+++ t1.py
@@ -0,0 +1,4 @@
+x = 27
+
+if x==27:
+    print ("Twenty Seven")
---
+++ t3.py
@@ -0,0 +1,30 @@
+#mylist = []
+#mylist.append(1)
+#mylist.append(2)
+#mylist.append(3)
+#print(mylist[0]) # prints 1
+#print(mylist[1]) # prints 2
+#print(mylist[2]) # prints 3
+
+# prints out 1,2,3
+#for x in mylist:
+#    print(x)
+
+# accessing an index which does not exist generates an exception (an error)
+#mylist = [1, 2, 3]
+#print(mylist[10])
+
+numbers = [1,2,3]
+strings = 'hello world'
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+
+
+second_name = 'Eric'
+
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
